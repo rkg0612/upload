ce2b653cbd87bbe0a4491bf6d173518472d0c7b2
Ralph Klein Guevarra <ralphkleinguevarra@gmail.com>
2022-06-14 07:15:31 +0800

    push

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function() {
+    return view('welcome');
+});
 
 Route::post('upload', [FileController::class, 'upload']);
